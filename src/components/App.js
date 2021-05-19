d169ea378150f57c88fc0e7fadd54ba72d6822db
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-05-19 15:30:12 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,42 @@ import itemData from "../data/items";
 function App() {
   const [items, setItems] = useState(itemData);
 
+
+  let [isToggleOn, setIsToggleOn] = useState(false)
+
   // this data will be passed down to the ShoppingList as a prop
   console.log(items);
 
-  return (
-    <div className={"App " + (false ? "dark" : "light")}>
-      <header>
+  let handleClick = () => {
+    setIsToggleOn(isToggleOn = !isToggleOn)
+} 
+
+if (isToggleOn) {
+  return (<div className="App dark">
+  <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>Dark Mode
+        </button>
       </header>
       <ShoppingList items={items} />
     </div>
-  );
+  
+  )
+  
+}
+else {
+
+ return (<div className="App light">
+    <header>
+        <h2>Shopster</h2>
+        <button onClick={handleClick}>Light Mode
+        </button>
+      </header>
+      <ShoppingList items={items} />
+    </div>
+      )
+  }
+
 }
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+  let [isInCart, setIsInCart] = useState(false)
+
+  let handleClick = () => {
+    setIsInCart(isInCart = !isInCart)
+} 
+
+if (isInCart){
   return (
-    <li className="">
+    <li className="in-cart">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">Add to Cart</button>
     </li>
   );
+}
+else {
+  return (
+  <li className="">
+      <span>{name}</span>
+      <span className="category">{category}</span>
+      <button onClick={handleClick} className="add">Add to Cart</button>
+    </li>
+        )
+    }
 }
 
 export default Item;
